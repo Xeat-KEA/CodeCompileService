caedb8accc67bb5177da7c9b195245fb71694ff4
gytjd_omk5aqq <gytjd9516@naver.com>
2024-10-08 18:48:06 +0900

    fix: json 타입 컬럼에 @Column(columnDefinition = "json") 추가

--- src/main/java/com/example/codecompileservice/entity/Code.java
+++ src/main/java/com/example/codecompileservice/entity/Code.java
@@ -1,6 +1,7 @@
 package com.example.codecompileservice.entity;
 
 import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import org.hibernate.annotations.Type;
@@ -12,5 +13,6 @@ public class Code {
     @Id
     private String id;
     @Type(JsonType.class)
+    @Column(columnDefinition = "json")
     private List<Testcase> testcases;
 }
