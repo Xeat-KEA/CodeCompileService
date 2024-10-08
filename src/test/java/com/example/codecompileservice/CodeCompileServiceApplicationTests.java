4c66ed544f0840ece88cc17f2e60937ea6cbb914
gytjd_omk5aqq <gytjd9516@naver.com>
2024-10-08 17:12:35 +0900

    "first commit"

---
+++ src/test/java/com/example/codecompileservice/CodeCompileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codecompileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCompileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
