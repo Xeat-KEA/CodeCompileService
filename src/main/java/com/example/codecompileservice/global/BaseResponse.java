ab230650c50e6953f522119762ce787655fbe015
gytjd_omk5aqq <gytjd9516@naver.com>
2024-10-08 18:36:25 +0900

    feat: 응답 형식 클래스 생성

--- src/main/java/com/example/codecompileservice/global/BaseResponse.java
+++ src/main/java/com/example/codecompileservice/global/BaseResponse.java
@@ -1,10 +1,22 @@
 package com.example.codecompileservice.global;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BaseResponse<T> {
     private Integer statusCode;
     private String message;
     private T data;
+
+    // 성공 응답 생성
+    public static <T> BaseResponse<T> success(T data) {
+        return new BaseResponse<>(200, "요청 성공", data);
+    }
+
+    // 성공 응답 생성
+    public static <T> BaseResponse<T> error(Integer statusCode, String message, T data) {
+        return new BaseResponse<>(statusCode, message, data);
+    }
 }
