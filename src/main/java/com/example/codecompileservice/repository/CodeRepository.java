381f0282f3d6bc248b658fb50fc943109e5d172f
gytjd_omk5aqq <gytjd9516@naver.com>
2024-10-08 22:59:16 +0900

    feat: 문제 테스트케이스 저장, 조회

--- src/main/java/com/example/codecompileservice/repository/CodeRepository.java
+++ src/main/java/com/example/codecompileservice/repository/CodeRepository.java
@@ -1,7 +1,10 @@
 package com.example.codecompileservice.repository;
 
+import com.example.codecompileservice.entity.Code;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CodeRepository {
+public interface CodeRepository extends JpaRepository<Code,Integer> {
+
 }
--- src/main/java/com/example/codecompileservice/service/CodeService.java
+++ src/main/java/com/example/codecompileservice/service/CodeService.java
@@ -1,10 +1,21 @@
 package com.example.codecompileservice.service;
 
+import com.example.codecompileservice.entity.Code;
+import com.example.codecompileservice.global.BaseResponse;
+import com.example.codecompileservice.repository.CodeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CodeService {
+    private final CodeRepository codeRepository;
 
+    public BaseResponse<Code> codeSave(Code code) {
+        return BaseResponse.success(codeRepository.save(code));
+    }
+
+    public BaseResponse<Code> codeFind(Integer id) {
+        return BaseResponse.success(codeRepository.findById(id).orElseThrow());
+    }
 }
