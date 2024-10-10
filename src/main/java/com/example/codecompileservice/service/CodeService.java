d03579c4a51709d1ce2750eaa23359a0e269356a
gytjd_omk5aqq <gytjd9516@naver.com>
2024-10-10 16:28:05 +0900

    refactor: 컴파일 요류 예외처리 CT-31

--- src/main/java/com/example/codecompileservice/service/CodeService.java
+++ src/main/java/com/example/codecompileservice/service/CodeService.java
@@ -16,11 +16,7 @@ import java.util.Base64;
 @RequiredArgsConstructor
 public class CodeService {
     private final CodeRepository codeRepository;
-    private final JavaExecutor javaExecutor;
-    private final JavaScriptExecutor jsExecutor;
-    private final PythonExecutor pythonExecutor;
-    private final CExecutor cExecutor;
-    private final CppExecutor cppExecutor;
+    private final CodeExecutor codeExecutor;
 
     public BaseResponse<Code> codeSave(Code code) {
         return BaseResponse.success(codeRepository.save(code));
@@ -32,35 +28,13 @@ public class CodeService {
 
     public BaseResponse<CodeCompileOutput> codeCompile(CodeCompileInput codeCompileInput) throws Exception {
         Code code = codeRepository.findById(codeCompileInput.getCodeId()).get();
-
-        return switch (codeCompileInput.getLanguage()) {
-            case "java" ->
-                    BaseResponse.success(new CodeCompileOutput(javaExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases())));
-            case "js" ->
-                    BaseResponse.success(new CodeCompileOutput(jsExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases())));
-            case "python" ->
-                    BaseResponse.success(new CodeCompileOutput(pythonExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases())));
-            case "c" ->
-                    BaseResponse.success(new CodeCompileOutput(cExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases())));
-            default ->
-                    BaseResponse.success(new CodeCompileOutput(cppExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases())));
-        };
+        return BaseResponse.success(new CodeCompileOutput(codeExecutor
+                .execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), codeCompileInput.getLanguage(), code.getTestcases())));
     }
 
     public BaseResponse<CodeSubmitOutput> codeSubmit(CodeCompileInput codeCompileInput) throws Exception {
         Code code = codeRepository.findById(codeCompileInput.getCodeId()).get();
-
-        return switch (codeCompileInput.getLanguage()) {
-            case "java" ->
-                    BaseResponse.success(new CodeSubmitOutput(code.grade(javaExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases()))));
-            case "js" ->
-                    BaseResponse.success(new CodeSubmitOutput(code.grade(jsExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases()))));
-            case "python" ->
-                    BaseResponse.success(new CodeSubmitOutput(code.grade(pythonExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases()))));
-            case "c" ->
-                    BaseResponse.success(new CodeSubmitOutput(code.grade(cExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases()))));
-            default ->
-                    BaseResponse.success(new CodeSubmitOutput(code.grade(cppExecutor.execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), code.getTestcases()))));
-        };
+        return BaseResponse.success(new CodeSubmitOutput(code.grade(codeExecutor
+                .execute(new String(Base64.getDecoder().decode(codeCompileInput.getCode())), codeCompileInput.getLanguage(), code.getTestcases()))));
     }
 }
